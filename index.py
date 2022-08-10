9db0ba5662a5077fd81be20f234c094c6470fde3
averyj123 <averykjones@icloud.com>
2022-08-10 14:47:06 -0700

    Initializing main file

---
+++ index.py
@@ -0,0 +1,16 @@
+"""
+steps:
+1. github
+  a. initialize in terminal
+  b. import to github website
+2. make txt files
+   a. create one for each of my classes
+   b. put in fake names as they would appear (w profile pic thing)
+   c. make the top line say the subject
+3. research file editing as a refresher
+4. go through each text file and remove profile pic line
+5. create a dictionary of people where the keys are the peoples names and the values are lists of the classes they have
+   a. use the subject name on the first line
+6. create a new dictionary with the same people but with the keys as the number of classes with them
+7. using the number value, rank them and list teh classes next to the name
+"""
